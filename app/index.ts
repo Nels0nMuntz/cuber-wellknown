f0b6f20b385255396b3eedd71388cd6692c414b5
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2024-11-12 20:18:55 +0200

    added redirect to the app

--- app/index.ts
+++ app/index.ts
@@ -4,15 +4,20 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-const APPLE_TEAM_ID = "APPLE_TEAM_ID";
-const IOS_BUNDLE_ID = "com.eugenechekerdes.cuber";
-const ANDROID_PACKAGE_NAME = "com.eugene_chekerdes.cuber";
-
 dotenv.config({
   path: path.join(__dirname, "..", `.env.${process.env.NODE_ENV}`),
 });
 
+const APPLE_TEAM_ID = "APPLE_TEAM_ID";
+const IOS_BUNDLE_ID = "com.eugenechekerdes.cuber";
+const ANDROID_PACKAGE_NAME = "com.eugene_chekerdes.cuber";
+const STATICS_PATH =
+  process.env.MODE === "production" ? "../../public/" : "../public/";
+
+
+
 const app = express();
+app.use(express.static("public"));
 app.use(
   cors({
     origin: "*",
@@ -51,7 +56,7 @@ app.get("/.well-known/assetlinks.json", (req, res) => {
           namespace: "android_app",
           package_name: ANDROID_PACKAGE_NAME,
           sha256_cert_fingerprints: [
-            "FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C",
+            "06:AD:ED:DF:15:C4:B2:4F:A7:14:12:85:F2:8C:44:07:76:3C:07:F7:55:CC:A4:8A:F3:44:CE:DE:1E:E4:D7:99",
           ],
         },
       },
@@ -60,13 +65,12 @@ app.get("/.well-known/assetlinks.json", (req, res) => {
   res.end();
 });
 
-app.use(express.static('public'))
-app.get("*", (req, res, next) => {
+app.get("/", (req, res, next) => {
   const title = "Cuber";
   const subtitle = "Find out more about Cuber app";
-  const image = "../../public/images/cuber-web-image.webp";
+  const image = `${STATICS_PATH}images/cuber-web-image.webp`;
 
-  const templatePath = path.join(__dirname, "../../public/html/index.html");
+  const templatePath = path.join(__dirname, `${STATICS_PATH}html/index.html`);
 
   var source = fs
     .readFileSync(templatePath, { encoding: "utf-8" })
@@ -76,6 +80,10 @@ app.get("*", (req, res, next) => {
 
   res.send(source);
   return;
+});
+
+app.get("*", (req, res) => {
+  res.send("Welcome!");
 });
 
 export default app;
